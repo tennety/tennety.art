a566aa62b95221a34bdb993c4bc208269122c7b4
Chandu Tennety <chandu.tennety@gmail.com>
2020-05-13 09:02:45 -0400

    Update palette
    ...not used yet, but should make it nice to switch light vs dark modes

--- gen/Pages.elm
+++ gen/Pages.elm
@@ -20,7 +20,7 @@ import Time
 
 builtAt : Time.Posix
 builtAt =
-    Time.millisToPosix 1587433135468
+    Time.millisToPosix 1589408138467
 
 
 type PathKey
--- src/Palette.elm
+++ src/Palette.elm
@@ -1,16 +1,30 @@
-module Palette exposing (blogHeading, color, heading, scaled)
+module Palette exposing (blogHeading, color, fromElmColor, heading, scaled)
 
+import Color
 import Element exposing (Element)
 import Element.Font as Font
 import Element.Region
 
 
 color =
-    { primary = Element.rgb255 5 117 230
-    , secondary = Element.rgb255 0 242 96
+    { lightest = Element.rgb255 0xF7 0xFA 0xFC
+    , lighter = Element.rgb255 0xE2 0xE8 0xF0
+    , light = Element.rgb255 0xCB 0xD5 0xE0
+    , neutral = Element.rgb255 0xA0 0xAE 0xC0
+    , dark = Element.rgb255 0x4A 0x55 0x68
+    , darker = Element.rgb255 0x2D 0x37 0x48
+    , darkest = Element.rgb255 0x1A 0x20 0x2C
     }
 
-scaled = (Element.modular 20 1.25) >> round
+
+fromElmColor : Color.Color -> Element.Color
+fromElmColor =
+    Color.toRgba >> Element.fromRgb
+
+
+scaled =
+    Element.modular 20 1.25 >> round
+
 
 heading : Int -> List (Element msg) -> Element msg
 heading level content =
