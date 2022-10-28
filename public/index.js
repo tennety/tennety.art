32098fa4d0ef75595fcd889acc9a4d2b8684aa57
Chandu Tennety <chandu.tennety@gmail.com>
2022-10-27 22:54:49 -0400

    swipe little-loader to get GA working

--- public/index.js
+++ public/index.js
@@ -1,8 +1,19 @@
+import loader from "/little-loader.js";
 /** @typedef {{load: (Promise<unknown>); flags: (unknown)}} ElmPagesInit */
 
 /** @type ElmPagesInit */
 export default {
   load: async function (elmLoaded) {
+    loader()
+    window._lload("https://www.googletagmanager.com/gtag/js?id=UA-157882374-1", () => {
+      console.log("pups")
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-157882374-1');
+    })
+
     const app = await elmLoaded;
     console.log("App loaded", app);
   },
